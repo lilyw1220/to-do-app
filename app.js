d8d366962724d1ce1c47996276b7dd72c5109cec
Lily Wu <lilyw1220@gmail.com>
2019-11-12 22:01:56 -0800

    TODOAPP

--- app.js
+++ app.js
@@ -16,11 +16,23 @@ function onReady() {
    // set the input's type to checkbox
    checkbox.type = "checkbox";
 
+   let deleteBtn = document.createElement("button");
+   deleteBtn.textContent = "Delete";
+
+   deleteBtn.addEventListener('click', function(event){
+     // console.log(event);
+     // this.parentElement represents the button's <li> parent
+     toDoList.removeChild(this.parentElement);
+   })
+
    // set the title
       newLi.textContent = title;
 
     // attach the checkbox to the li
       newLi.appendChild(checkbox);
+
+    // delete button
+      newLi.appendChild(deleteBtn);
 
     // attach the li to the ul
       toDoList.appendChild(newLi);
